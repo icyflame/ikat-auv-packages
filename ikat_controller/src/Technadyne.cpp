2443afc6c879d21287053a4f558ccab76f564e8d
sidd.piku <sidd.piku@gmail.com>
2012-08-31 16:21:11 +0000

    chnages made to manifext.xml and cmakeist.txt

---
+++ ikat_controller/src/Technadyne.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <ikat_controller/Technadyne.h>
+#include <string.h>
+#include <iostream>
+#include <cstring>
+
+
+#define ADD1 0x01
+#define ADD2 0x02
+
+
+thruster::thruster(const std::string &port,int baudrate=9600):Serial(port,baudrate)
+{
+}
+
+bool thruster::startThrusters()
+{
+    return Serial.openPort();
+}
+
+thruster::~thruster()
+{
+    Serial.~IkatSerialPort();
+}
+
+void thruster::strcmb(double a,int b)
+{
+    thv=a;
+    chan=b;
+    str.clear();
+    char buff[20];
+    memset(buff,0,sizeof(buff));
+    if(thv>5.0)
+    {
+        printf("thruster voltage crossed +5.0 !!!\n");
+        thv=5.0;
+    }
+    if(thv<-5.0)
+    {
+        printf("thruster voltage crossed -5.0 !!!\n");
+        thv=-5.0;
+    }
+    if(thv>=0.0)
+    {
+        byteswritten=sprintf(buff,"#0%dC%d-0%.3lf\r",ADD1,chan,thv);
+    }
+    else
+    {   
+        byteswritten=sprintf(buff,"#0%dC%d-0%.3lf\r",ADD1,chan,-thv);
+    }
+    str = buff;
+}
+
+bool thruster::sendData(void)
+{
+    if(Serial.writeData(str)==byteswritten)
+    {
+        return true;
+    }
+    return false;
+}
