778b9182ef67e6f36c3ce6a220f0518ccfb4b04e
vinayyadav3016@gmail.com <vinayyadav3016@gmail.com@086d8ec9-eb77-9479-a919-13724bd1ee23>
2012-09-03 17:39:05 +0000

    I have created thruster codes. @ madhukar please check the syntex of the thruster_no
    So tomorrow I suppose we will be ready with controller :)

---
+++ trunk/ikat_thrusters/include/ikat_thrusters/Thrusters.hpp
@@ -0,0 +1,35 @@
+#ifndef THRUSTER_H
+#define THRUSTER_H
+
+#include <ros/ros.h>
+#include <serialport/IkatSerialPort.h>
+#include <cstring>
+#include <string.h>
+
+
+namespace ikat_hardware
+{
+  #define THRUSTER_V_MIN -5.0
+  #define THRUSTER_V_MAX +5.0
+  #define ADD1 0x01
+  #define ADD2 0x02
+  #define ADD3 0x03
+  class Thruster
+  {
+    private :
+      char thruster_data[20];
+      ikat_sensor::IkatSerialPort serial_port;
+      void setData(int thruster_no, float voltage);
+    public :
+      Thruster(std::string &port_name);
+      Thruster(std::string &port_name,int baud_rate);
+      bool connectThrusters();
+      bool disconnectThrusters();
+      bool sendData(int thruster_no, float voltage);
+      bool haultAllThruster();
+      virtual ~Thruster();
+  };
+}// end namespace ikat_hardware
+
+
+#endif
---
+++ trunk/ikat_thrusters/src/test.cpp
@@ -0,0 +1,27 @@
+#include <ros/ros.h>
+#include <ikat_thrusters/Thrusters.hpp>
+
+//#define DEPTH_DEBUG 1
+
+int main(int argc, char** argv)
+{
+  ros::init(argc,argv,"thruster_test_code");
+#ifdef DEPTH_DEBUG
+  log4cxx::LoggerPtr my_logger = log4cxx::Logger::getLogger(ROSCONSOLE_DEFAULT_NAME);
+  my_logger->setLevel(ros::console::g_level_lookup[ros::console::levels::Debug]);
+#endif
+  std::string port_name = " ";
+  ikat_hardware::Thruster auv_thrusters(port_name,9600);
+  if(auv_thrusters.connectThrusters())
+  {
+      ROS_DEBUG_STREAM("thruster are online to use");
+      auv_thrusters.sendData(0,2.5);
+      auv_thrusters.sendData(1,2.5);
+      auv_thrusters.sendData(2,2.5);
+      sleep(2);
+      auv_thrusters.haultAllThruster();
+  }
+  else
+      ROS_ERROR_STREAM("Error while conneting thruster");
+  return 0;
+}
