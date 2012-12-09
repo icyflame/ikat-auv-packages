c1844eadcdf11802e81b84633858bdc6f4f739b0
Vinay Yadav <vinayyadav3016@users.noreply.github.com>
2012-12-09 21:54:50 +0000

    UWSim folder is here copy this folder in UWSim direcotry. Use UWSim_auv.launch file to launch application.

--- auv_visulaization/src/setSimulator.cpp
+++ auv_visulaization/src/setSimulator.cpp
@@ -5,10 +5,19 @@ int main(int argc,char** argv)
 {
     ros::init(argc,argv,"setSimulator_client");
     ros::NodeHandle n;
-    ros::ServiceClient client = n.serviceClient
-            <gazebo::DeleteModel>("delete_model");
+    //ros::ServiceClient client = n.serviceClient
+            //<gazebo::DeleteModel>("deleteModel");
     gazebo::DeleteModel del_model;
     del_model.request.model_name = "plane1_model";
-    client.call(del_model);
+    ros::service::call("gazebo/delete_model",del_model);
+    if(del_model.response.success)
+    {
+        ROS_INFO_STREAM("Yo YO");
+    }
+    else
+    {
+        ROS_INFO_STREAM("Fuck");
+    }
+    //
     return 0;
 }
